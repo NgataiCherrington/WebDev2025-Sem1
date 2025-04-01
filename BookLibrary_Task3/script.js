55016bb58e70a782f2613a03ce1422577b6e03d1
chernj1 <chernj1@D202-78647>
2025-04-01 15:43:19 +1300

    uploading this task to github repo

---
+++ BookLibrary_Task3/script.js
@@ -0,0 +1,28 @@
+const books = [
+    {
+        name: "The Night Circus",
+        author: "Erin Morgenstern",
+        genre: "Fantasy"
+    },
+    {
+        name: "1984",
+        author: "George Orwell",
+        genre: "Fiction"
+    },
+    {
+        name: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        genre: "Fiction"
+    },
+    {
+        name: "The Black Company",
+        author: "Glen Cook",
+        genre: "Fantasy"
+    },
+    {
+        name: "The Broken Empire Trilogy",
+        author: "Mark Lawrence",
+        genre: "Fantasy"
+    }   
+]
+
