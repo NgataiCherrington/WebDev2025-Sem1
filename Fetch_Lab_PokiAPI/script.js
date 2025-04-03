5707e1cb1b8a8f377081858e26788ad986621788
chernj1 <chernj1@D202-78647>
2025-04-03 14:48:21 +1300

    updating before class

--- Fetch_Lab_PokiAPI/script.js
+++ Fetch_Lab_PokiAPI/script.js
@@ -1,18 +1,57 @@
+const pokemonWikiLinks = [
+  {
+    //link for bulbasaur 
+    link: "https://bulbapedia.bulbagarden.net/wiki/Bulbasaur_(Pok%C3%A9mon)"
+  },
+  {
+    //link for ivysaur
+    link: "https://bulbapedia.bulbagarden.net/wiki/Ivysaur_(Pok%C3%A9mon)"
+  },
+  {
+    //link for venusaur
+    link: "https://bulbapedia.bulbagarden.net/wiki/Venusaur_(Pok%C3%A9mon)"
+  },
+  {
+    link: "https://pokemondb.net/pokedex/charmander"
+  },
+  {
+    link: "https://pokemondb.net/pokedex/charmeleon"
+  },
+  {
+    link: "https://pokemondb.net/pokedex/charizard"
+  },
+  {
+    link: "https://pokemondb.net/pokedex/squirtle"
+  },
+  {
+    link: "https://pokemondb.net/pokedex/wartortle"
+  },
+  {
+    link: "https://pokemondb.net/pokedex/blastoise"
+  },
+  {
+    link: "https://pokemondb.net/pokedex/caterpie"
+  }
+
+]
+
 
 async function fetchPokemon() {
   const url = "https://pokeapi.co/api/v2/pokemon";
   const response = await fetch(url);
   const pokemon = await response.json();
 
-  pokemonData(pokemon)
+  pokemonData(pokemon.results, pokemonWikiLinks)
 }
 
-function pokemonData(data) {
+
+
+function pokemonData(data, pokemonWikiLinks) {
     const pokemonContainer = document.getElementById("section");
     for (let i = 0; i < 10; i++) {
         const myPokemon = `
         <h3>Name: ${data.results[i].name}</h3>
-        <a href="${data.results[i].url}" targe="_blank">Click this link</a>
+        <a href="${pokemonWikiLinks[i].link}" target="_blank">Click this link</a>
         `
         pokemonContainer.innerHTML += myPokemon;
     }
