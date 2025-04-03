2a4c09266f72a52d9475cb78454b7a4550ad6f0d
chernj1 <chernj1@D202-78647>
2025-04-03 17:14:44 +1300

    lab finished

--- Fetch_Lab_PokiAPI/script.js
+++ Fetch_Lab_PokiAPI/script.js
@@ -1,60 +1,52 @@
-const pokemonWikiLinks = [
-  {
-    //link for bulbasaur 
-    link: "https://bulbapedia.bulbagarden.net/wiki/Bulbasaur_(Pok%C3%A9mon)"
-  },
-  {
-    //link for ivysaur
-    link: "https://bulbapedia.bulbagarden.net/wiki/Ivysaur_(Pok%C3%A9mon)"
-  },
-  {
-    //link for venusaur
-    link: "https://bulbapedia.bulbagarden.net/wiki/Venusaur_(Pok%C3%A9mon)"
-  },
-  {
-    link: "https://pokemondb.net/pokedex/charmander"
-  },
-  {
-    link: "https://pokemondb.net/pokedex/charmeleon"
-  },
-  {
-    link: "https://pokemondb.net/pokedex/charizard"
-  },
-  {
-    link: "https://pokemondb.net/pokedex/squirtle"
-  },
-  {
-    link: "https://pokemondb.net/pokedex/wartortle"
-  },
-  {
-    link: "https://pokemondb.net/pokedex/blastoise"
-  },
-  {
-    link: "https://pokemondb.net/pokedex/caterpie"
-  }
+const container = document.querySelector("section");
 
-]
 
+async function displayPokemonInfo(url) {
+  const response = await fetch(url);
+  const pokeInfo = await response.json();
+  
+  console.log(pokeInfo);
+}
 
 async function fetchPokemon() {
-  const url = "https://pokeapi.co/api/v2/pokemon";
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=12&offset=600";
   const response = await fetch(url);
   const pokemon = await response.json();
-
-  pokemonData(pokemon.results, pokemonWikiLinks)
+  
+  for (let poke of pokemon.results) {
+    fetchPokemonDetails(poke);
+  };
 }
 
-
-
-function pokemonData(data, pokemonWikiLinks) {
-    const pokemonContainer = document.getElementById("section");
-    for (let i = 0; i < 10; i++) {
-        const myPokemon = `
-        <h3>Name: ${data.results[i].name}</h3>
-        <a href="${pokemonWikiLinks[i].link}" target="_blank">Click this link</a>
-        `
-        pokemonContainer.innerHTML += myPokemon;
+async function fetchPokemonDetails(pokemon) {
+  const response = await fetch(pokemon.url);
+  const details = await response.json();
+  
+  displayPokemon (
+    {
+      name: details.name,
+      image: details.sprites.front_default,
+      abilities: details.abilities.map(a => a.ability.name).join(", "),
+      types: details.types.map(t => t.type.name).join(", "),
+      stats: details.stats.map(s => `${s.stat.name}: ${s.base_stat}`).join(", ")
     }
+  );
 }
 
+function displayPokemon(pokemon) {
+  const myPokemon = document.createElement("div");
+  myPokemon.classList.add("pokemon-card");
+
+  myPokemon.innerHTML = `
+  <h2>${pokemon.name}</h2>
+  <img src="${pokemon.image}" alt="${pokemon.name}">
+  <p><strong>Type:</strong> ${pokemon.types}</p>
+  <p><strong>Ability:</strong> ${pokemon.abilities}</p>
+  <p><strong>Stats:</strong> ${pokemon.stats}</p>
+  `;
+
+  myPokemon.addEventListener("click", () => console.log("Detailed Pokemon Info:", pokemon));
+  
+  container.appendChild(myPokemon);
+}
 fetchPokemon();
